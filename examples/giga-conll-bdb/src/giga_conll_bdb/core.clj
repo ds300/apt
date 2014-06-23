aa134b772173ff94120903d0898276b145688fe6
David Sheldrick <ds300@sussex.ac.uk>
2014-06-23 15:31:31 +0100

    fix up extraction task

--- examples/giga-conll-bdb/src/giga_conll_bdb/core.clj
+++ examples/giga-conll-bdb/src/giga_conll_bdb/core.clj
@@ -1,9 +1,11 @@
 (ns giga-conll-bdb.core
   (:gen-class)
-  (:import [uk.ac.susx.tag.apt AccumulativeAPTStore$Builder RGraph])
-  (:require [tag.apt.backend.db :refer [bdb-lexicon]]
+  (:import [uk.ac.susx.tag.apt AccumulativeAPTStore$Builder RGraph]
+           [com.sleepycat.je EnvironmentConfig])
+  (:require [tag.apt.backend.db :refer [bdb-lexicon *env-config*]]
             [tag.apt.util :refer [gz-reader pmapall]]
-            [tag.apt.conll :refer [parse]]))
+            [tag.apt.conll :refer [parse]]
+            [clojure.pprint :refer [pprint]]))
 
 (def pos-map
 {  "JJ", "J"
@@ -60,33 +62,50 @@
       (report! n))))
 
 (defn to-graph [tkn-index dep-index sent]
-  (let [graph (RGraph. (count sent))
+  (let [numTokens (count sent)
+        graph (RGraph. numTokens)
         ids   (.entityIds graph)]
     (doseq [[idx word lemma pos gov dep] sent]
       (when (and gov dep)
-        (let [tkn-id (tkn-index (str (if (= pos "CD") "NUMBER" lemma) "/" (pos-map pos pos)))
+        (let [tkn-id (tkn-index (str (if (= pos "CD") "NUMBER" (.toLowerCase ^String lemma)) "/" (pos-map pos pos)))
               dep-id (dep-index dep)
               idx (Integer. idx)
               gov (Integer. gov)]
-          (aset ids idx tkn-id)
-          (.addRelation graph idx gov dep-id))))
+            (aset ids idx tkn-id)
+            (.addRelation graph idx gov dep-id))))
     graph))
 
+(def num-files (atom 0))
+
+(defmacro nothrow [& body]
+  `(try (do ~@body)
+     (catch Throwable e# nil)))
 
 (defn process-file! [lexicon file]
+  (swap! num-files inc)
+  (println "Processing file: " (.getAbsolutePath file))
   (let [tkn-index (.getEntityIndex lexicon)
         dep-index (.getRelationIndex lexicon)]
     (with-open [in (gz-reader file (.length file))]
       (doseq [sent (parse in)]
-        (.include lexicon (to-graph tkn-index dep-index sent))
-        (done-sent!)))))
+        (if-let [graph (nothrow (to-graph tkn-index dep-index sent))]
+          (do
+            (.include lexicon graph)
+            (done-sent!))
+          (do 
+            (println "Failed to build graph for:")
+            (pprint sent)))))))
 
 
 (defn -main
   "I don't do a whole lot ... yet."
   [home dbname depth & input-files]
   (let [store-builder (.setMaxDepth (AccumulativeAPTStore$Builder.) (Integer. depth))]
+    (binding [*env-config* (doto (EnvironmentConfig.)
+                                (.setAllowCreate true)
+                                (.setLocking false))]
     (with-open [lexicon (bdb-lexicon home dbname store-builder)]
       (dorun (pmapall (partial process-file! lexicon) (map clojure.java.io/as-file input-files)))
       (shutdown-agents)
-      (report! @num-sents))))
+      (report! @num-sents)
+      (println "Number of files processed: " @num-files)))))
