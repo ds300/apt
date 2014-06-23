2fb10ccf62b351058bda699ca123624105a90ea9
David Sheldrick <ds300@sussex.ac.uk>
2014-06-23 15:32:39 +0100

    comment out integration tests

--- test/tag/apt/test/conll.clj
+++ test/tag/apt/test/conll.clj
@@ -121,4 +121,5 @@ nothing
                            (fn [sent] (.include lexicon (to-graph tkn-index dep-index sent)))
                            (parse in)))))))
 
-(time (do-berkeley-test))
+;(time (do-berkeley-test))
+;(time (do-integration-test))
